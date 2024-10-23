dbc1ad74752f08492a23193be994803667541422
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2024-10-23 13:56:46 +0900

    Update 체스판다시칠하기.py

--- 코딩테스트 대비 문제집 2레벨/체스판다시칠하기.py
+++ 코딩테스트 대비 문제집 2레벨/체스판다시칠하기.py
@@ -4,15 +4,32 @@ n, m = map(int, input().split())
 board = []
 ans = 64
 
+#board에 각 문자를 2차원 배열로 입력하기기
 for _ in range(n):
-    temp = input()
-    s = []
-    for t in temp:
-        s.append(t)
-    board.append(s)
+    board.append(list(input()))
 
 for i in range(n-7):
     for j in range(m-7):
+        b_ans = 0 #black으로 시작하는 경우
+        w_ans = 0 #white로 시작하는 경우
+        b_start = 'B'
         for x in board[i:i+8]:
-            print(x[j:j+8])
-        print()
+            for y in range(8):
+                #black으로 시작한 경우, y가 짝수일 때 !b_start이면 => b_ans+1
+                #white로 시작한 경우, y가 짝수일 때 ==b_start이면 => w_ans+1
+                if y%2==0:
+                    if x[j+y]!=b_start:
+                        b_ans+=1
+                    else:
+                        w_ans+=1
+                else:
+                    if x[j+y]==b_start:
+                        b_ans+=1
+                    else:
+                        w_ans+=1
+            if b_start == 'B':
+                b_start = 'W'
+            else:
+                b_start = 'B'
+        ans = min([b_ans, w_ans, ans])
+print(ans)
