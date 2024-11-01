1fad7088bb71a72bffef260bbef6f947576d2875
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2024-11-01 17:51:34 +0900

    [Silver III] Title: 1로 만들기, Time: 556 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,13 @@
 from sys import stdin
 
-n = int(stdin.readline())
-d = [0]*1000001
+x = int(stdin.readline())
+dp = [0]*(x+1) #1~x까지 각 1까지 가기 위해서 필요한 연산 최소 횟수를 담는 배열열
 
-for i in range(2, n+1):
-    d[i] = d[i-1]+1
+for i in range(2, x+1):
+    cnt = dp[i-1]+1
+    if i%3==0: #3으로 나뉘는 경우
+        cnt = min(cnt, dp[i//3]+1)
     if i%2==0:
-        d[i]=min(d[i], d[i//2]+1)
-    if i%3==0:
-        d[i]=min(d[i], d[i//3]+1)
-        
-print(d[n])
+        cnt = min(cnt, dp[i//2]+1)
+    dp[i] = cnt
+print(dp[x])
