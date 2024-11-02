30a30c937ceae0482dcf50b9ab7de7ba9f6bf720
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2024-11-03 00:53:28 +0900

    [Silver V] Title: 돌 게임 2, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+n = int(stdin.readline())
+
+turn = 'SK' #'SK'와 'CY'를 번갈아 가면서 할당함
+dp = [1001]*(n+1)
+dp[0] = 0
+
+for i in range(n+1):
+    if i+1<=n:
+        dp[i+1] = min(dp[i]+1, dp[i+1])
+    if i+3<=n:
+        dp[i+3] = min(dp[i]+1, dp[i+3])
+if dp[n]%2==1:
+    print('CY')
+else:
+    print('SK')
