c45bd9790141df0f4bc66ead9c70fbc9e1972308
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2024-12-23 09:30:44 +0900

    [Silver III] Title: 빈도 정렬, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -0,0 +1,18 @@
+from sys import stdin
+from collections import Counter
+
+#[숫자: [빈도 수, 처음 등장한 인덱스]]
+
+n, c = map(int, stdin.readline().split())
+arr = list(map(int, stdin.readline().split()))
+answer = {}
+
+for i in range(n):
+    if arr[i] not in answer:
+        answer[arr[i]] = [0, i]
+    answer[arr[i]][0]+=1
+
+answer = sorted(answer.items(), key=lambda x: (-x[1][0], x[1][1]))
+for a in answer:
+    for _ in range(a[1][0]):
+        print(a[0], end=' ')
