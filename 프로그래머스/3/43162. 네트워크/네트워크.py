bf686a70588ba9e41509565566d199526e9f1274
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2025-02-04 17:18:17 +0900

    [level 3] Title: 네트워크, Time: 0.60 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,22 @@
+'''
+영역 개수 구하기 문제
+'''
+
+def solution(n, computers):
+    answer = 0
+    
+    visited = [False for _ in range(n)]
+    
+    def dfs(curr):
+        for nxt in range(n):
+            if computers[curr][nxt]==1 and not visited[nxt]:
+                visited[nxt] = True
+                dfs(nxt)
+    
+    for curr in range(n):
+        if not visited[curr]:
+            answer+=1
+            visited[curr] = True
+            dfs(curr)
+    
+    return answer
