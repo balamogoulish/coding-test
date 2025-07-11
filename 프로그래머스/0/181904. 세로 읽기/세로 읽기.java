af2f2b107c65d00fd911d8e3060c6f5de5143b00
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2025-07-11 18:06:53 +0900

    [level 0] Title: 세로 읽기, Time: 11.40 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -2,9 +2,10 @@ class Solution {
     public String solution(String my_string, int m, int c) {
         String answer = "";
         int str_len = my_string.length();
+        char[] charArr = my_string.toCharArray();
         for(int i=0; i<str_len; i++){
             if((i+1)%m == c%m){
-                answer+=my_string.substring(i,i+1);
+                answer+=charArr[i];
             }
         }
         return answer;
