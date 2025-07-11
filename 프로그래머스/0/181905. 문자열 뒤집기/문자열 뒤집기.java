5dc31f3c902185db00500a32cb8a065fdf548065
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2025-07-11 17:38:21 +0900

    [level 0] Title: 문자열 뒤집기, Time: 20.77 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,20 +1,12 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String my_string, int s, int e) {
-        String answer = "";
+    public String solution(String my_string, int s, int e) {        
         
+        StringBuilder mid = new StringBuilder(my_string.substring(s,e+1)).reverse();
         String prev = my_string.substring(0, s);
-        String mid = my_string.substring(s,e+1);
         String nxt = my_string.substring(e+1);
         
-        answer+=prev;
-        char[] mid_arr= mid.toCharArray();
-        for(int i=0; i<mid_arr.length; i++){
-            answer+=mid_arr[mid_arr.length-i-1];
-        }
-        answer+=nxt;
-        
-        return answer;
+        return prev+mid.toString()+nxt;
     }
 }
