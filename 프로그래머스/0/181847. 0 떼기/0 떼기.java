357d98298cd1dbe7de684260c6517834260182d9
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2025-08-08 22:43:32 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int n_int = Integer.parseInt(n_str);
+        return Integer.toString(n_int);
+    }
+}
