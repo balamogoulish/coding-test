23b196ab6657121a368e2fb7b7498b98714a453c
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2025-09-02 14:11:39 +0900

    [Silver IV] Title: 카드2, Time: 232 ms, Memory: 56772 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Queue<Integer> cards = new LinkedList<>();
+        for(int i=1;i<=N;i++){
+            cards.add(i);
+        }
+
+        while(cards.size()>1){
+            cards.poll();
+            int next = cards.poll();
+            cards.add(next);
+        }
+        System.out.println(cards.peek());
+    }
+}
