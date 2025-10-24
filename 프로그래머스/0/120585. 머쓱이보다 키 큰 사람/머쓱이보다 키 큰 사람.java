17510a93039fae3fceef14055e4a6d120093ca9d
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2025-10-24 21:27:55 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.48 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        Arrays.sort(array);
+        
+        for(int i=0; i<array.length; i++){
+            if(array[array.length-1-i] > height) answer++;
+        }
+        return answer;
+    }
+}
