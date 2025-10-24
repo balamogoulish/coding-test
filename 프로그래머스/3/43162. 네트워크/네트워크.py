6960e569577775c77e0e3d9a8cc1e260d98179d7
SeongEun Gwak <109948529+balamogoulish@users.noreply.github.com>
2025-10-25 00:25:57 +0900

    [level 3] Title: 네트워크, Time: 0.59 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,8 +1,5 @@
 '''
 영역 개수 구하기 문제
-'''
-
-def solution(n, computers):
     answer = 0
     
     visited = [False for _ in range(n)]
@@ -20,3 +17,24 @@ def solution(n, computers):
             dfs(curr)
     
     return answer
+'''
+
+def solution(n, computers):
+    answer = 0
+    
+    visited = [False for _ in range(n)];
+    
+    def dfs(curr):
+        for nxt in range(n):
+            if computers[curr][nxt]==1 and not visited[nxt]:
+                visited[nxt] = True
+                dfs(nxt)
+    
+    for curr in range(n):
+        if not visited[curr]:
+            answer+=1
+            dfs(curr)
+    
+    return answer
+    
+    
